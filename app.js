a637027e1b19dcb6093d727bedc5483cddee8f9c
virajdaxini2510 <daxini.viraj@gmail.com>
2026-04-06 22:13:16 +0530

    New commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('CI/CD Node.js App is Running!');
