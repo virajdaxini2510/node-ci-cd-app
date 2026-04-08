982da1af8678fcd821993a6378fd0b8b7d2002fa
virajdaxini2510 <daxini.viraj@gmail.com>
2026-04-08 13:24:00 +0530

    6EC1 commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('CI/CD Node.js App is Running!');
 });
+app.get('/home', (req, res) => {
+    res.send('CI/CD Node.js App is Running from Home!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
